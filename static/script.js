4b709ce5d841f752a3b3a93e8c76473d185e8760
p.sumara <sumara0498@icloud.com>
2021-05-07 14:16:20 +0200

    Refactored code to use React components that are  displaying questions from database

--- static/script.js
+++ static/script.js
@@ -3,37 +3,3 @@ window.onload = function () {
     selector: ".background",
   });
 };
-
-var question = document.querySelector(".question");
-var nextBtn = document.querySelector(".next-btn");
-var previousBtn = document.querySelector(".previous-btn");
-
-var axisX = 0;
-
-var hideControl = function hideControl() {
-  if (axisX === -280) {
-    nextBtn.classList.add("hideControl");
-    console.log(axisX);
-  } else {
-    nextBtn.classList.remove("hideControl");
-  }
-
-  if (axisX === 0) {
-    previousBtn.classList.add("hideControl");
-  } else {
-    previousBtn.classList.remove("hideControl");
-  }
-};
-
-hideControl();
-
-nextBtn.addEventListener("click", function () {
-  question.style.marginLeft = (axisX -= 140) + "%";
-  console.log(axisX);
-  hideControl();
-});
-
-previousBtn.addEventListener("click", function () {
-  question.style.marginLeft = (axisX += 140) + "%";
-  hideControl();
-});
